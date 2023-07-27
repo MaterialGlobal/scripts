6c77fa514fd78dd27f50999f57ee3b26f3907dae
honk <github@honk.email>
2023-07-27 03:03:06 +0200

    fix: lots of patches to the code

--- site/nc/startup.sh
+++ site/nc/startup.sh
@@ -13,7 +13,7 @@ function image_fail() {
 function warn_java() {
     echo "WARNING: Please ensure the Docker Image in the Startup tab is set to one of the Java images."
     echo "WARNING: Otherwise, your App Container will not run at all."
-    read -p "Type 'yes' if it's set to Java or 'no' if it's not: " option
+    read -rp "Type 'yes' if it's set to Java or 'no' if it's not: " option
     case $option in
         yes) run_java ;;
         no) image_fail ;;
@@ -25,7 +25,7 @@ function warn_java() {
 function warn_node() {
     echo "WARNING: Please ensure the Docker Image in the Startup tab is set to one of the NodeJS images."
     echo "WARNING: Otherwise, your App Container will not run at all."
-    read -p "Type 'yes' if it's set to NodeJS or 'no' if it's not: " option
+    read -rp "Type 'yes' if it's set to NodeJS or 'no' if it's not: " option
     case $option in
         yes) run_node ;;
         no) image_fail ;;
@@ -37,7 +37,7 @@ function warn_node() {
 function warn_python() {
     echo "WARNING: Please ensure the Docker Image in the Startup tab is set to one of the Python images."
     echo "WARNING: Otherwise, your App Container will not run at all."
-    read -p "Type 'yes' if it's set to Python or 'no' if it's not: " option
+    read -rp "Type 'yes' if it's set to Python or 'no' if it's not: " option
     case $option in
         yes) run_python ;;
         no) image_fail ;;
@@ -47,35 +47,35 @@ function warn_python() {
 
 # Function to run the Java command
 function run_java() {
-    java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar ${JARFILE}
+    java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar "${JARFILE}"
 }
 
 # Function to run the Node.js command
 function run_node() {
     if [[ -d .git ]]; then git pull; fi
-    if [[ ! -z "${NODE_PACKAGES}" ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi
+    if [[ -n "${NODE_PACKAGES}" ]]; then /usr/local/bin/npm install "${NODE_PACKAGES}"; fi
     if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi
-    /usr/local/bin/node /home/container/{{JS_FILE}}
+    /usr/local/bin/node /home/container/"${JS_FILE}"
 }
 
 # Function to run the Python command
 function run_python() {
-    if [[ -d .git ]] && [[ "{{AUTO_UPDATE}}" == "1" ]]; then git pull; fi
-    if [[ ! -z "${PY_PACKAGES}" ]]; then pip install -U --prefix .local ${PY_PACKAGES}; fi
-    if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then pip install -U --prefix .local -r ${REQUIREMENTS_FILE}; fi
-    /usr/local/bin/python /home/container/{{PY_FILE}}
+    if [[ -d .git ]]; then git pull; fi
+    if [[ -n "${PY_PACKAGES}" ]]; then pip install -U --prefix .local "${PY_PACKAGES}"; fi
+    if [[ -f /home/container/${REQUIREMENTS_FILE} ]]; then pip install -U --prefix .local -r "${REQUIREMENTS_FILE}"; fi
+    /usr/local/bin/python /home/container/"${PY_FILE}"
 }
 
-if [ $installstate == "java" ]; then
-    run_java()
+if [ "$installstate" == "java" ]; then
+    run_java
 fi
 
-if [ $installstate == "node" ]; then
-    run_node()
+if [ "$installstate" == "node" ]; then
+    run_node
 fi
 
-if [ $installstate == "python" ]; then
-    run_python()
+if [ "$installstate" == "python" ]; then
+    run_python
 fi
 
 # Menu options
@@ -86,7 +86,7 @@ echo "2) Run NodeJS"
 echo "3) Run Python"
 
 # Read user input
-read -p "Enter option number: " option
+read -rp "Enter option number: " option
 
 # Execute the selected option
 case $option in
